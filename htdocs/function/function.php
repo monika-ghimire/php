a12ad10950ea7bce2b96588445478fa4e529647d
monika ghimire <ghimirey.monika@gmail.com>
2024-03-27 18:35:51 +0545

    Your descriptive commit message here

---
+++ htdocs/function/function.php
@@ -0,0 +1,13 @@
+<?php 
+
+$name = "monika" ;
+$age = 21;
+$nameList = array("monika" , "sonika");
+//it is auto dedeteced so what ever come after = it will find
+
+echo "my name is $name and AGE is $age"  ; 
+echo var_dump($nameList);
+
+echo $nameList[1];
+
+?>
---
+++ htdocs/harry/testagian/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "agin new floder";
+?>
